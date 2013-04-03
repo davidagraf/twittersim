0d48dc99774d4a2dfc8a1610cbc16af53a26999b
David Graf <davidagraf@gmail.com>
2013-04-03 07:40:44 +0200

    wip - switch to coffeescript

--- Gruntfile.js
+++ Gruntfile.js
@@ -48,7 +48,7 @@ module.exports = function (grunt) {
         options: {
           port: 9000,
           // Change this to '0.0.0.0' to access the server from outside.
-          hostname: 'localhost',
+          hostname: '0.0.0.0',
           middleware: function (connect) {
             return [
               lrSnippet,
@@ -97,7 +97,7 @@ module.exports = function (grunt) {
     coffee: {
       dist: {
         files: {
-          '.tmp/scripts/coffee.js': '<%= yeoman.app %>/scripts/*.coffee'
+          '.tmp/scripts/scripts.js': '<%= yeoman.app %>/scripts/{,*/}*.coffee'
         }
       },
       test: {
@@ -232,8 +232,6 @@ module.exports = function (grunt) {
   });
 
   grunt.renameTask('regarde', 'watch');
-  // remove when mincss task is renamed
-  grunt.renameTask('mincss', 'cssmin');
 
   grunt.registerTask('server', [
     'clean:server',
@@ -256,7 +254,7 @@ module.exports = function (grunt) {
   grunt.registerTask('build', [
     'clean:dist',
     'jshint',
-    'test',
+//    'test',
     'coffee',
     'compass:dist',
     'useminPrepare',
